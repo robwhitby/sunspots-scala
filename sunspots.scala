31655447544b166ab823fafcdabd61370e363862
Rob Whitby <robwhitby78@yahoo.co.uk>
2012-03-24 14:29:05 +0000

    sunspots

---
+++ sunspots.scala
@@ -0,0 +1,17 @@
+#!/bin/sh
+exec scala "$0" "$@"
+!#
+
+case class Point(x: Int, y: Int, value: Int) {
+  def toString(score: Int) = "(%s,%s score:%s)".format(x, y, score)
+}
+
+def sortedWithScore = points.map(p => (p, area(p).map(_.value).sum)).sortBy(_._2).reverse
+
+def area(mid: Point) = points.filter(p => (mid.x-1 to mid.x+1).contains(p.x) && (mid.y-1 to mid.y+1).contains(p.y))
+
+val Array(top, size, values @ _*) = args.map(_.toInt)
+
+val points = for((value, i) <- values.zipWithIndex) yield Point(i % size, i / size, value)
+
+println(sortedWithScore.take(top).map{ case(p,s) => p.toString(s) }.mkString)
---
+++ test.sh
@@ -0,0 +1,5 @@
+echo "expect: (3,3 score:26)"
+echo "actual: $(./sunspots.scala 1 5 5 3 1 2 0 4 1 1 3 2 2 3 2 4 3 0 2 3 3 2 1 0 2 4 3)"
+echo
+echo "expect: (1,2 score:27)(1,1 score:25)(2,2 score:23)" 
+echo "actual: $(./sunspots.scala 3 4 2 3 2 1 4 4 2 0 3 4 1 1 2 3 4 4)"
